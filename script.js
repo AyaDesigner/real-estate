ab92872b16b8c02885cf43c4e46ef2e7b9b11f83
AyaDesigner <aya.berdyeva@gmail.com>
2020-11-01 12:09:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,179 @@
+const houses = [
+    {
+        name: "Country house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text house 1 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Vacation house",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Rolling_Tiny_House_mit_Pelletofen_auf_PKW-Anh%C3%A4nger.jpg",
+        description: "Text 2 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Sea house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 3 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Wooden house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 4 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Country house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text house 1 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Vacation house",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Rolling_Tiny_House_mit_Pelletofen_auf_PKW-Anh%C3%A4nger.jpg",
+        description: "Text 2 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Sea house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 3 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Wooden house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 4 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Country house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text house 1 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Vacation house",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Rolling_Tiny_House_mit_Pelletofen_auf_PKW-Anh%C3%A4nger.jpg",
+        description: "Text 2 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Sea house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 3 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Wooden house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 4 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Country house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text house 1 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Vacation house",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Rolling_Tiny_House_mit_Pelletofen_auf_PKW-Anh%C3%A4nger.jpg",
+        description: "Text 2 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Sea house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 3 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Wooden house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 4 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Country house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text house 1 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Vacation house",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Rolling_Tiny_House_mit_Pelletofen_auf_PKW-Anh%C3%A4nger.jpg",
+        description: "Text 2 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Sea house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 3 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    },
+    {
+        name: "Wooden house",
+        image: "https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg",
+        description: "Text 4 description provided by the architects. The design of the housing is born focusing on the style of the Ibizan country house, simple clear lines, controlled light and the white color as the starting stroke. The house is dampened by the light of the city of Dénia, located in an environment in which the accent has to be placed only inside, having to divert attention from what surrounds it. "
+    }
+];
+
+
+{/* 
+<div class="col-sm">
+   
+    
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://cf.bstatic.com/images/hotel/max1024x768/255/255830738.jpg" alt="Card image cap">
+                
+            <div class="card-body">
+                <h5 class="card-title">Country house</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">3 bathrooms</li>
+                <li class="list-group-item">Garden</li>
+                <li class="list-group-item">2 garages</li>
+            </ul>
+            
+        </div> 
+</div> 
+
+
+*/}
+
+
+function displayHouse(house, rowHouse) {
+
+
+
+    const column = document.createElement("div");
+    column.classList.add("col");
+    rowHouse.appendChild(column);
+
+
+
+    const houseCard = document.createElement("div");
+    houseCard.classList.add("card");
+    houseCard.style = "width: 18rem;";
+    column.appendChild(houseCard);
+
+
+    const cardImg = document.createElement("img");
+    cardImg.classList.add("card-img-top");
+    cardImg.src = house.image;
+    houseCard.appendChild(cardImg);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    houseCard.appendChild(cardBody);
+
+    const cardHeading = document.createElement("h5");
+    cardHeading.classList.add("card-title");
+    const cardText = document.createTextNode(house.name);
+    cardHeading.appendChild(cardText);
+    cardBody.appendChild(cardHeading);
+
+    const descrip = document.createElement("p");
+    const descripText = document.createTextNode(house.description);
+    descrip.appendChild(descripText);
+    cardBody.appendChild(descrip);
+
+
+}
+
+const housesContainer = document.querySelector("#houses-container");
+
+const row = document.createElement("div");
+row.classList.add("row");
+
+housesContainer.appendChild(row);
+
+
+
+for (let i = 0; i < houses.length; i++) {
+    displayHouse(houses[i], row);
+}
